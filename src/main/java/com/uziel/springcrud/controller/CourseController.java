eda97b722ecd0f32b550b6ceec3e0abeac3de70c
Uziel Menezes <uzielcivil@gmail.com>
2023-07-27 08:11:58 -0300

    feat: new controller method to find course by id

--- src/main/java/com/uziel/springcrud/controller/CourseController.java
+++ src/main/java/com/uziel/springcrud/controller/CourseController.java
@@ -8,7 +8,9 @@ import com.uziel.springcrud.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,14 +26,22 @@ public class CourseController {
 
     // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public List<Course> list() {
+    public List<Course> listAllCourses() {
         return courseRepository.findAll();
+    }
+
+    // PathVariable can pass the identifier in case the parameter doesn't match name
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findCourseById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course) {
+    public Course createCourse(@RequestBody Course course) {
         return courseRepository.save(course);
         // return
         // ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));

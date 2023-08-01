fa8a8981ca243c9fedc388b3f6fdfe0c11f20e4e
Uziel Menezes <uzielcivil@gmail.com>
2023-07-31 21:41:26 -0300

    feat: update method in controller

--- src/main/java/com/uziel/springcrud/controller/CourseController.java
+++ src/main/java/com/uziel/springcrud/controller/CourseController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -34,7 +35,7 @@ public class CourseController {
     @GetMapping("/{id}")
     public ResponseEntity<Course> findCourseById(@PathVariable Long id) {
         return courseRepository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
+                .map(recordValue -> ResponseEntity.ok().body(recordValue))
                 .orElse(ResponseEntity.notFound().build());
     }
 
@@ -45,6 +46,19 @@ public class CourseController {
         return courseRepository.save(course);
         // return
         // ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+
+        return courseRepository.findById(id)
+                .map(recordValue -> {
+                    recordValue.setName(course.getName());
+                    recordValue.setCategory(course.getCategory());
+                    Course updateValue = courseRepository.save(recordValue);
+                    return ResponseEntity.ok().body(updateValue);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }

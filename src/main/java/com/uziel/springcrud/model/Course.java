cdc05e3fd553dcab80499abd9490303822a8966f
Uziel Menezes <uzielcivil@gmail.com>
2023-10-21 11:27:01 -0300

    feat: validators as annotation

--- src/main/java/com/uziel/springcrud/model/Course.java
+++ src/main/java/com/uziel/springcrud/model/Course.java
@@ -1,5 +1,7 @@
 package com.uziel.springcrud.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -17,9 +22,16 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 3, max = 50)
+    @Column(length = 50, nullable = false, unique = true)
     private String name;
 
+    @NotBlank
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Back-End|Front-End")
     @Column(length = 10, nullable = false)
     private String category;
 

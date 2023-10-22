86d2609411c1b98199b4379ce2ae097b42894df6
Uziel Menezes <uzielcivil@gmail.com>
2023-10-22 07:31:47 -0300

    feat: soft delete and mapped search

--- src/main/java/com/uziel/springcrud/model/Course.java
+++ src/main/java/com/uziel/springcrud/model/Course.java
@@ -1,8 +1,8 @@
 package com.uziel.springcrud.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,6 +16,8 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id = ?")
+@Where(clause = "status = 'Active'")
 public class Course {
 
     @Id
@@ -35,4 +37,10 @@ public class Course {
     @Column(length = 10, nullable = false)
     private String category;
 
+    @NotBlank
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Active|Inactive")
+    @Column(length = 10, nullable = false)
+    private String status = "Active";
 }

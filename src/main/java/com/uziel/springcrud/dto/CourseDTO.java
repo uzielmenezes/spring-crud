a528b9851bf283234cb3b0c14b07063bcb9a65b7
Uziel <uzielcivil@gmail.com>
2024-02-23 12:33:58 -0300

    chore: indentation

--- src/main/java/com/uziel/springcrud/dto/CourseDTO.java
+++ src/main/java/com/uziel/springcrud/dto/CourseDTO.java
@@ -7,8 +7,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
-        Long id,
-        @NotBlank @NotNull @Length(min = 3, max = 50) String name,
-        @NotBlank @NotNull @Length(max = 10) @Pattern(regexp = "Back-End|Front-End") String category) {
+                Long id,
+                @NotBlank @NotNull @Length(min = 3, max = 50) String name,
+                @NotBlank @NotNull @Length(max = 10) @Pattern(regexp = "Back-End|Front-End") String category) {
 
 }

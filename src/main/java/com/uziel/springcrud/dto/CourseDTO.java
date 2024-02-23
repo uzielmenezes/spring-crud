b654fd6c9fa77da9aec167b455ff5221bf8f0fa0
Uziel <uzielcivil@gmail.com>
2024-02-23 12:58:55 -0300

    feat: enum validator

--- src/main/java/com/uziel/springcrud/dto/CourseDTO.java
+++ src/main/java/com/uziel/springcrud/dto/CourseDTO.java
@@ -2,13 +2,15 @@ package com.uziel.springcrud.dto;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.uziel.springcrud.enums.Category;
+import com.uziel.springcrud.enums.validation.ValueOfEnum;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
                 Long id,
                 @NotBlank @NotNull @Length(min = 3, max = 50) String name,
-                @NotBlank @NotNull @Length(max = 10) @Pattern(regexp = "Back-End|Front-End") String category) {
+                @NotBlank @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category) {
 
 }

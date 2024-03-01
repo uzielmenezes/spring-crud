ec4fbf8c01330644e76b0ae46645d00cfca8dea2
Uziel <uzielcivil@gmail.com>
2024-03-01 08:26:21 -0300

    chore: adding a new lesson on initialization

--- src/main/java/com/uziel/springcrud/SpringCrudApplication.java
+++ src/main/java/com/uziel/springcrud/SpringCrudApplication.java
@@ -32,6 +32,12 @@ public class SpringCrudApplication {
 			l.setCourse(c);
 			c.getLessons().add(l);
 
+			Lesson l1 = new Lesson();
+			l1.setName("Angular");
+			l1.setYoutubeUrl("watch?=2");
+			l1.setCourse(c);
+			c.getLessons().add(l1);
+
 			courseRepository.save(c);
 		};
 	}

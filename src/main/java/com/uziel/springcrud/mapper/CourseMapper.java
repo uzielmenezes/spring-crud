bcc03446bae7491c0919a05f4e46344a57b90e05
Uziel <uzielcivil@gmail.com>
2024-03-01 08:51:15 -0300

    feat: adding lessons list to CourseMapper method

--- src/main/java/com/uziel/springcrud/mapper/CourseMapper.java
+++ src/main/java/com/uziel/springcrud/mapper/CourseMapper.java
@@ -1,8 +1,11 @@
 package com.uziel.springcrud.mapper;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.uziel.springcrud.dto.CourseDTO;
+import com.uziel.springcrud.dto.LessonDTO;
 import com.uziel.springcrud.enums.Category;
 import com.uziel.springcrud.model.Course;
 
@@ -12,7 +15,13 @@ import jakarta.validation.constraints.NotNull;
 public class CourseMapper {
 
     public CourseDTO toDTO(@NotNull Course course) {
-        return new CourseDTO(course.getId(), course.getName(), course.getCategory().getValue());
+        List<LessonDTO> lessons = course.getLessons()
+                .stream()
+                .map(lesson -> new LessonDTO(lesson.getId(), lesson.getName(), lesson.getYoutubeUrl()))
+                .toList();
+
+        return new CourseDTO(course.getId(), course.getName(), course.getCategory().getValue(),
+                lessons);
     }
 
     public Course toEntity(@NotNull CourseDTO courseDTO) {
